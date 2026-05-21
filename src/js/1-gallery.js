0c312eab072f06fe75abbf534a2ce1011d919f94
Darman <saintenough1992@gmail.com>
2026-05-21 08:31:13 +0300

    added some markup changes

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -71,11 +71,11 @@ const gallery = document.querySelector('.gallery');
 const markup = images
   .map(
     image => `<li class="gallery-item">
-        <a class="gallery-link" href=${image.original}>
+        <a class="gallery-link" href="${image.original}">
             <img
             class="gallery-image"
-            src=${image.preview}
-            alt=${image.description}
+            src="${image.preview}"
+            alt="${image.description}"
             />
         </a>
     </li>`
