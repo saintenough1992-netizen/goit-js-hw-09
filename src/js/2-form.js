d04d1cd81ab8396887723d6bb870c00ad1807dcf
Darman <saintenough1992@gmail.com>
2026-05-21 13:38:05 +0300

    played with the form to add tracking of the previous message and email

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,13 +2,21 @@ const form = document.querySelector('.feedback-form');
 const emailInput = form.elements.email;
 const messageInput = form.elements.message;
 const storageKey = 'feedback-form-state';
+const prevEmail = document.querySelector('.prev-email');
+const prevMessage = document.querySelector('.prev-message');
+const prevKey = 'feedback-form-prev';
+const clearCache = document.querySelector('.clear-cache');
 
 let formData = {
   email: '',
   message: '',
 };
 const savedData = localStorage.getItem(storageKey);
-
+const prevData = localStorage.getItem(prevKey);
+if (prevData) {
+  prevEmail.textContent = JSON.parse(prevData).email || '';
+  prevMessage.textContent = JSON.parse(prevData).message || '';
+}
 if (savedData) {
   formData = JSON.parse(savedData);
 
@@ -32,10 +40,19 @@ form.addEventListener('submit', e => {
     return;
   }
   console.log(formData);
+  localStorage.setItem(prevKey, JSON.stringify(formData));
+  prevEmail.textContent = formData.email;
+  prevMessage.textContent = formData.message;
   localStorage.removeItem(storageKey);
   formData = {
     email: '',
     message: '',
   };
   form.reset();
+});
+clearCache.addEventListener('click', e => {
+  e.preventDefault();
+  localStorage.removeItem(prevKey);
+  prevEmail.textContent = '';
+  prevMessage.textContent = '';
 });
